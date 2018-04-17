b5aec34b812551da336dcc6d63746c8460ab6a8b
Eric Cheung <eric1357a@users.noreply.github.com>
2018-04-17 21:57:54 +0800

    Added map

--- app/src/main/java/app/sarc/com/sarc/ViewController/Adapter/RestaurantAdapter.java
+++ app/src/main/java/app/sarc/com/sarc/ViewController/Adapter/RestaurantAdapter.java
@@ -31,40 +31,29 @@ import app.sarc.com.sarc.ViewController.RestaurantDetailView.RestaurantDetailAct
  * Created by Aron on 26/3/2018.
  */
 
-public class RestaurantAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
 
     private ArrayList<RestaurantInfo> restaurantInfos;
 
     public RestaurantAdapter(ArrayList<RestaurantInfo> restaurantInfos) {
         this.restaurantInfos = restaurantInfos;
     }
-
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-         switch (viewType){
-                     case 1:
-                         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_item_view, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_item_view, parent, false);
                          return new ViewHolder(itemView);
-                     case 2:
-                         itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.map_view, parent,false);
-                         return new ViewHolder(itemView);
-                 }
-                 return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-        switch (holder.getItemViewType()){
-            case 1:
-                ViewHolder rest_view =  (ViewHolder) holder;
-                rest_view.RES_Name.setText(restaurantInfos.get(position).name);
-                rest_view.RES_Description.setText(restaurantInfos.get(position).description);
-                 Glide.with(holder.itemView.getContext())
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.RES_Name.setText(restaurantInfos.get(position).name);
+        holder.RES_Description.setText(restaurantInfos.get(position).description);
+        Glide.with(holder.itemView.getContext())
                 .load(Constants.baseURL+restaurantInfos.get(position).imgSrc)
                 .thumbnail(Glide.with(holder.itemView.getContext()).load(R.drawable.loading_pre_view))
                 .fitCenter()
                 .crossFade()
-                .into(rest_view.RES_Image);
+                .into(holder.RES_Image);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,43 +62,6 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                 v.getContext().startActivity(intent);
             }
         });
-                break;
-            case 2:
-                mapHolder mapHolder = (mapHolder) holder;
-
-                break;
-        }
-    }
-
-
-
-
-//    @Override
-//    public void onBindViewHolder(final ViewHolder holder, final int position) {
-//        holder.RES_Name.setText(restaurantInfos.get(position).name);
-//        holder.RES_Description.setText(restaurantInfos.get(position).description);
-//        Glide.with(holder.itemView.getContext())
-//                .load(Constants.baseURL+restaurantInfos.get(position).imgSrc)
-//                .thumbnail(Glide.with(holder.itemView.getContext()).load(R.drawable.loading_pre_view))
-//                .fitCenter()
-//                .crossFade()
-//                .into(holder.RES_Image);
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), RestaurantDetailActivity.class);
-//                intent.putExtra("ITEMVALUE", restaurantInfos.get(position));
-//                v.getContext().startActivity(intent);
-//            }
-//        });
-//    }
-
-    @Override
-    public int getItemViewType(int position) {
-              int viewType = 1;
-              if(position == 0) viewType = 2;
-              else viewType = 1;
-              return viewType;
     }
 
     @Override
@@ -131,39 +83,5 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     }
 
-    public class mapHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback {
-        private GoogleMap mMap;
-        MapView map;
-        LatLng mMapLocation;
-        Context context;
 
-        public mapHolder(View itemView) {
-            super(itemView);
-            map = itemView.findViewById(R.id.mapView);
-            this.context = itemView.getContext();
-            if (map != null) {
-                map.onCreate(null);
-                map.onResume();
-                map.getMapAsync(this);
-            }
-        }
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            MapsInitializer.initialize(context);
-            mMap = googleMap;
-        }
-
-        public void setMapLocation(LatLng mapLocation) {
-            mMapLocation = mapLocation;
-            if (mMap != null) {
-                updateMapContents();
-            }
-        }
-
-        protected void updateMapContents() {
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(mMapLocation));
-        }
-    }
 }
--- app/src/main/java/app/sarc/com/sarc/ViewController/MainActivity.java
+++ app/src/main/java/app/sarc/com/sarc/ViewController/MainActivity.java
@@ -1,12 +1,28 @@
 package app.sarc.com.sarc.ViewController;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -15,28 +31,133 @@ import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import java.util.ArrayList;
 
 import app.sarc.com.sarc.Common.NetworkingMethod.APIResponse;
+import app.sarc.com.sarc.Model.RestaurantInfo;
 import app.sarc.com.sarc.Model.RestaurantList;
 import app.sarc.com.sarc.R;
 import app.sarc.com.sarc.ViewController.Adapter.RestaurantAdapter;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
 
     private RecyclerView RestaurantListView;
     private RestaurantAdapter adapter;
-    private GoogleMap mMap;
+    //define an array to store the Json
+    private ArrayList<RestaurantInfo> restaurantInfos;
+    //private MapFragment mapFragment;
+
+    private MapView mapView;
+    private GoogleMap gmap;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private UiSettings uiSettings;
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Restaurant List");
+       // mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+
+       // mapFragment.getMapAsync(MainActivity.this);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(mapViewBundle);//you need to add the key
+        mapView.getMapAsync(this);
+
+
         RestaurantListView = findViewById(R.id.Res_list);
         RestaurantListView.setHasFixedSize(true);
         RestaurantListView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         InitImageLoader();
         getRestaurantListData();
+    }
+    private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (gmap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                gmap.setMyLocationEnabled(true);
+                gmap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                gmap.setMyLocationEnabled(false);
+                gmap.getUiSettings().setMyLocationButtonEnabled(false);
+                //mLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        gmap = map;// don't hide, this object needs to be define
+        uiSettings = map.getUiSettings();
+        LatLng home = new LatLng(22.335501, 114.182890);
+        MapsInitializer.initialize(getApplicationContext());
+       uiSettings.setAllGesturesEnabled(true);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(home)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        gmap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//        map.addMarker(new MarkerOptions()
+//                .position(new LatLng(0, 0))
+//                .title("Marker"));
+    }
+
+    private void addMarker(){
+        for (RestaurantInfo restaurantInfo_ : restaurantInfos){
+            gmap.addMarker(new MarkerOptions()
+            .position(new LatLng(Float.parseFloat(restaurantInfo_.latitude), Float.parseFloat(restaurantInfo_.longitude)))
+            .title(restaurantInfo_.name));
+            Log.e("TAG!!!!!!!!!",restaurantInfo_.name);//same as CONSOLE
+        }
     }
 
     private void InitImageLoader(){
@@ -52,7 +173,9 @@ public class MainActivity extends AppCompatActivity {
         // Initialize ImageLoader with configuration.
         ImageLoader.getInstance().init(config.build());
     }
+private void GoogleMapOptions(){
 
+}
     private void getRestaurantListData(){
         new APIResponse().getRestaurantList(new Observer<RestaurantList>() {
             @Override
@@ -62,8 +185,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNext(RestaurantList restaurantList) {
+                //the data got from server when OnNext method was accessed
+
+                restaurantInfos = restaurantList.restaturant_list;
                 adapter = new RestaurantAdapter(restaurantList.restaturant_list);
                 RestaurantListView.setAdapter(adapter);
+                addMarker();
             }
 
             @Override
@@ -77,4 +204,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
